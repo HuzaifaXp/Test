df9b4f82ca112fb9da18260115391f7a51546770
HuzaifaXp <sardarhuzaifa891@gmail.com>
2023-01-07 10:20:20 +0100

    source code added

---
+++ aws_proj/src/components/NavBar.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { DbService } from '../services/DbService';
+import MensDress from '../pages/Categories/SelectedCategory/CategoriesBox';
+import Carousel from '../pages/Home/carousel'
+import './NavBar.css'
+import './navAnimate.css'
+import Cart from './Cart/Cart';
+import { List } from '@material-ui/core';
+
+function NavBar({itemsInCart,wishListSize}) {
+    const[catClass,setCatClass] = useState("show");
+    const[categories,setCategories] = useState({
+        loading: false,
+        categoriesList: [],
+        errMsg : ''
+    });
+   
+    const {loading,categoriesList,errMsg} = categories;    
+useEffect(async()=>{
+   
+    // setCategories({loading :false})
+    // const categoriesList =  async()=>{
+    //         const resp = await DbService.GetCategories();
+    //         return resp;
+    // }
+    
+    // var list = await categoriesList();
+    // setCategories({loading :false})
+    // setCategories({...categories,categoriesList : list});
+    // console.log('navbar page loaded', list)
+    
+
+},[]);
+
+    
+  return (
+    <div>
+           {/* <!-- Topbar Start --> */}
+   
+    
+    <div className="container-fluid mb-5 menu navFadeIn">
+        <div className="row border-top px-xl-5">
+            <div className="col-lg-12">
+                <nav className="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 menu">
+                   
+                    <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div className="navbar-nav mr-auto py-0">
+                     
+                <Link to="/" className="text-decoration-none">
+                    <h1 className="m-0 display-5 font-weight-semi-bold"><span className="text-primary font-weight-bold  px-3 mr-1">itsMS</span>Smith</h1>
+                </Link>
+                
+            
+                            
+                            <div className="nav-item dropdown">
+                                <a href="#" className="nav-link dropdown-toggle cat-btn" data-toggle="dropdown">Categories</a>
+                                <div className="dropdown-menu rounded-0 m-0">
+                                    
+
+                                    
+                                    <Link to="/Categories"className="dropdown-item" >Watches</Link>
+                                    <Link to="/Categories"className="dropdown-item" >Mic Podcasts </Link>
+                                    <Link to="/Categories"className="dropdown-item" >Baby's Dresses</Link>
+                                    
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="col-lg-3 col-6 text-right">
+                <Link to="/wishList" className="btn border">
+                    <i className="fas fa-heart text-primary"></i>
+                    <span className="badge">{wishListSize}</span>
+                </Link>
+                <Link  to={"/cart"} className="btn border" >
+                    <i className="fas fa-shopping-cart text-primary cart-icon"></i>
+                    
+                   {itemsInCart}
+                </Link>
+            </div>
+                    </div>
+                </nav>
+              
+            </div>
+           
+
+         
+        </div>
+    </div>
+       
+   
+                {/* { homepage && <Carousel />   }
+                { !homepage && <MensDress />   } */}
+
+    </div>
+  )
+}
+
+export default NavBar
